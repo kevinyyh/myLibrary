a2c4902e9b57f54cb067fcb2b527d3de25133190
kevinyyh <kevinyangyuhao@gmail.com>
2024-07-28 14:44:55 +0800

    complete first version of myLibrary project

---
+++ script.js
@@ -0,0 +1,163 @@
+const myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+let row = 0;
+function displayLibrary() {
+    if (myLibrary.length === 0) {
+        bookContainer.style.display = "none";
+        return;
+    }
+    bookContainer.style.display = "table";
+    bookshelf.innerHTML = null;
+    let row = Math.ceil(myLibrary.length / 5);
+    let currentIndex = 0;
+    for (let i = 0; i < row; i++) {
+        let newRow = bookshelf.insertRow();
+        for (let j = 0; j < 5; j++) {
+            let cell = newRow.insertCell(j);
+            if (i * 5 + j < myLibrary.length) {
+                cell.style.border = "1px solid black";
+                let container = document.createElement("div");
+                let title = document.createElement("div");
+                container.appendChild(title);
+                title.textContent = myLibrary[i * 5 + j].title;
+                let author = document.createElement("div");
+                container.appendChild(author);
+                author.textContent = myLibrary[i * 5 + j].author;
+                let pages = document.createElement("div");
+                container.appendChild(pages);
+                pages.textContent = myLibrary[i * 5 + j].pages + " pages";
+                let hasRead = document.createElement("div");
+                container.appendChild(hasRead);
+                hasRead.textContent = myLibrary[i * 5 + j].hasRead ? "has read" : "not read yet";
+                container.style.fontSize = "20px";
+                container.style.textAlign = "center";
+                cell.appendChild(container);
+            }
+        }
+    }
+}
+
+const bookContainer = document.querySelector(".bookContainer");
+const bookshelf = document.querySelector("tbody");
+const tableContainer = document.querySelector(".tableContainer");
+const mainContainer = document.querySelector(".mainContainer");
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+addBookToLibrary(new Book("Title", "Author", 300, true));
+displayLibrary();
+
+let addBook = document.querySelector(".addBook");
+addBook.addEventListener("click", () => {
+    addBook.disabled = true;
+    const bookFormContainer = document.createElement("div");
+    const bookForm = document.createElement("form");
+    bookForm.className = "bookForm";
+    bookForm.style.margin = "10px";
+    bookFormContainer.style.borderStyle = "solid";
+    bookFormContainer.style.borderColor = "black";
+    bookFormContainer.style.borderWidth = "1px";
+    bookFormContainer.appendChild(bookForm);
+    let input1 = addInputWithLabel("text", "title", "Enter the book's title:");
+    let input2 = addInputWithLabel("text", "author", "Enter the book's author:");
+    let input3 = addInputWithLabel("number", "pages", "Enter the book's total page:");
+    let input4 = addRadioButtonWithOptions("Yes", "No");
+    bookForm.appendChild(input1);
+    bookForm.appendChild(input2);
+    bookForm.appendChild(input3);
+    bookForm.appendChild(input4);
+    input1 = input1.querySelector("input");
+    input2 = input2.querySelector("input");
+    input3 = input3.querySelector("input");
+    input4 = input4.querySelector("#radio0");
+    console.log(input4);
+    const cancelButton = document.createElement("button");
+    cancelButton.textContent = "Cancel";
+    cancelButton.style.marginRight = "10px";
+    cancelButton.style.marginTop = "10px";
+    cancelButton.type = "button";
+    const submitButton = document.createElement("button");
+    submitButton.textContent = "Submit";
+    submitButton.style.marginTop = "10px";
+    bookForm.appendChild(cancelButton);
+    bookForm.appendChild(submitButton);
+    cancelButton.addEventListener("click", (e) => {
+        addBook.disabled = false;
+        bookFormContainer.remove();
+    });
+    submitButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (input1.checkValidity() && input2.checkValidity() && input3.checkValidity() && input4.checkValidity()) {
+            addBookToLibrary(new Book(input1.value, input2.value, input3.value, input4.checked));
+            displayLibrary();
+            bookFormContainer.remove();
+            addBook.disabled = false;
+        } else {
+            alert("fill in all required field");
+        }
+    });
+    mainContainer.appendChild(bookFormContainer);
+});
+
+function addInputWithLabel(inputType, id, labelName, inputCount) {
+    const input = document.createElement("input");
+    input.type = inputType;
+    input.id = id;
+    input.required = true;
+    const label = document.createElement("label");
+    label.style.display = "block";
+    label.style.marginBottom = "3px";
+    input.style.marginBottom = "10px";
+    label.id = id;
+    label.textContent = labelName;
+    const inputContainer = document.createElement("div");
+    inputContainer.appendChild(label);
+    inputContainer.appendChild(input);
+    return inputContainer;
+}
+
+function addRadioButtonWithOptions(...options) {
+    const radioContainer = document.createElement("div");
+    const question = document.createElement("p");
+    question.textContent = "Have you read this book? ";
+    question.style.marginTop = 0;
+    question.style.marginBottom = "3px";
+    radioContainer.appendChild(question);
+    for (let i = 0; i < options.length; i++) {
+        const radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = "hasRead";
+        radio.id = "radio" + i;
+        console.log(radio.id);
+        radio.value = options[i];
+        radio.required = true;
+        const label = document.createElement("label");
+        label.textContent = options[i];
+        radio.style.marginRight = "20px";
+        radioContainer.appendChild(label);
+        radioContainer.appendChild(radio);
+    }
+    return radioContainer;
+}
+
+
